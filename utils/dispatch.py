892115b17308b5f28e80cc878cb793ee5a9aeea0
coffee243454 <you@example.com>
2026-06-04 13:00:48 +0100

    Hyprland dispatch fix(my hyprland version is too old)

--- utils/dispatch.py
+++ utils/dispatch.py
@@ -1,13 +1,14 @@
 import os
 from fabric.utils import exec_shell_command_async, DesktopApp
 
-
 def dispatch_app(app: DesktopApp) -> bool:
-    cmd = app.executable  # or app.name if executable isn't available
+    cmd = app.executable
     if os.getenv("NIRI_SOCKET"):
         exec_shell_command_async(f"niri msg action spawn -- {cmd}")
     elif os.getenv("HYPRLAND_INSTANCE_SIGNATURE"):
-        exec_shell_command_async(f"hyprctl dispatch exec {cmd}")
+        exec_shell_command_async(
+            f'hyprctl dispatch \'hl.dsp.exec_cmd("{cmd}")\''
+        )
     elif os.getenv("MANGO_INSTANCE_SIGNATURE"):
         exec_shell_command_async(f"mmsg dispatch spawn, {cmd}")
     else:
