b5002e8ee84c08185252da3ba30b54380ee34e52
coffee243454 <you@example.com>
2026-06-05 22:03:00 +0100

    this works

--- utils/dispatch.py
+++ utils/dispatch.py
@@ -1,8 +1,12 @@
 import os
 from fabric.utils import exec_shell_command_async, DesktopApp
+import re
+
+def clean_cmd(cmd: str) -> str:
+    return re.sub(r'%\w', '', cmd).strip()
 
 def dispatch_app(app: DesktopApp) -> bool:
-    cmd = app.command_line
+    cmd = clean_cmd(app.command_line)
     if os.getenv("NIRI_SOCKET"):
         exec_shell_command_async(f"niri msg action spawn -- {cmd}")
     elif os.getenv("HYPRLAND_INSTANCE_SIGNATURE"):
